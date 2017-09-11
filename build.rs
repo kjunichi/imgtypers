366d687b2d9ade21445bf0f1d02ae69dd70d8911
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-09-12 07:27:20 +0900

    Fix lib path

--- build.rs
+++ build.rs
@@ -7,9 +7,10 @@ use std::io::Write;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_lib_path = Path::new(&out_dir).join("../../../deps").join("imgtype.lib");
     let dest_dll_path = Path::new(&out_dir).join("../../..").join("imgtype.dll");
     fs::copy("./vendor/imgtype.dll", dest_dll_path);
-    fs::copy("./vendor/imgtype.lib", out_dir);
+    fs::copy("./vendor/imgtype.lib", dest_lib_path);
 
-    println!("cargo:rustc-link-search=native=./vendor");
+    //println!("cargo:rustc-link-search=native=./vendor");
 }
