97b4c08c66ae5f4456405134cfb3395d38bf3dcf
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-09-29 16:01:44 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
+#[cfg(target_os = "linux")]
+use std::process::Command;
+
 #[cfg(target_os = "windows")]
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -20,5 +23,23 @@ fn main() {
     let _ = fs::create_dir(Path::new(&out_dir).join("lib"));
     let _ = fs::copy("./vendor/libimgtype.a.macos", dest_lib_path);
 
+    println!("cargo:rustc-link-search=native={}/lib", out_dir);
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    // git clone https://github.com/kjunichi/libimgtype.git
+    Command::new("git").args(&["clone","https://github.com/kjunichi/libimgtype.git"])
+    .status().unwrap();
+    // cd libimgtype
+    // go build -buildmode=c-archive -o libimgtype.a main.go
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_lib_path = Path::new(&out_dir).join("lib");
+    
+    Command::new("go").args(&["build","-buildmode=c-archive","-o"])
+    .arg(&format!("{}/libimgtype.a", dest_lib_path))
+    .current_dir(libimgtype)
+    .status().unwrap();
+    
     println!("cargo:rustc-link-search=native={}/lib", out_dir);
 }
